ee7bceda800451500b0ba3c7cfe44a5c132d1cf3
bladeau <al.almaktoum@live.com>
2020-10-23 09:25:28 +1100

    GraphQL without express example GraphQL without express example

---
+++ server/gql-server.js
@@ -0,0 +1,29 @@
+const { ApolloServer } = require('apollo-server');
+
+require('dotenv').config();
+
+// graphql server
+// types query/ mutation / subscription
+const typeDefs = `
+type Query{
+  totalPosts: Int!
+}
+`;
+
+// resolvers
+const resolvers = {
+  Query: {
+    totalPosts: () => 42
+  }
+};
+
+// graphql server
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+// port
+apolloServer.listen(process.env.PORT, () =>
+  console.log(`graphql server is ready at PORT:${process.env.PORT}`)
+);
--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const express = require('express');
 
-require("dotenv").config();
+require('dotenv').config();
 
 //express server
 const app = express();
 
 //rest endpoint
-app.get("/rest", (req, res) => {
-  res.json({ data: "you hit rest endpoint" });
+app.get('/rest', (req, res) => {
+  res.json({ data: 'you hit rest endpoint' });
 });
 
 // port
