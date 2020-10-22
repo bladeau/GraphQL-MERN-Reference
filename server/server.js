7c67fe6de935f2831e950b14b367faaeeb9f2d5e
bladeau <al.almaktoum@live.com>
2020-10-23 09:43:58 +1100

    Setup HTTP Server for Express and GraphQL together Setup HTTP Server for Express and GraphQL together

--- server/server.js
+++ server/server.js
@@ -1,9 +1,38 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const http = require('http');
 
-require('dotenv').config();
+const PORT = 8000;
 
 //express server
 const app = express();
+
+// graphql server
+// types query/ mutation / subscription
+const typeDefs = `
+type Query{
+  totalPosts: Int!
+}
+`;
+
+// resolvers
+const resolvers = {
+  Query: {
+    totalPosts: () => 42
+  }
+};
+
+// graphql server
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+// applyMiddleware method connects ApolloServer to a specific HTTP framework i.e. express
+apolloServer.applyMiddleware({ app: app });
+
+// server
+const httpserver = http.createServer(app);
 
 //rest endpoint
 app.get('/rest', (req, res) => {
@@ -11,6 +40,9 @@ app.get('/rest', (req, res) => {
 });
 
 // port
-app.listen(process.env.PORT, () =>
-  console.log(`server is ready at PORT:${process.env.PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`server is ready at PORT:${PORT}`);
+  console.log(
+    `graphql server is ready at PORT:${PORT}.... ${apolloServer.graphqlPath}`
+  );
+});
