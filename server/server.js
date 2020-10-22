e8b84eac6427e9e7865c128340a89d6f1a75b65f
bladeau <al.almaktoum@live.com>
2020-10-23 09:05:43 +1100

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+require("dotenv").config();
+
+//express server
+const app = express();
+
+//rest endpoint
+app.get("/rest", (req, res) => {
+  res.json({ data: "you hit rest endpoint" });
+});
+
+// port
+app.listen(process.env.PORT, () =>
+  console.log(`server is ready at PORT:${process.env.PORT}`)
+);
